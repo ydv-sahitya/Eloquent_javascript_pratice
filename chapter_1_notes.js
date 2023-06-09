b64d5d2e4130418f367d577eead7ec5688856f09
ydv-sahitya <11sahitya@gmail.com>
2023-06-09 19:11:13 +0530

    first commit

---
+++ chapter_1_notes.js
@@ -0,0 +1,46 @@
+// value, type , And Operators
+
+
+-- values 
+
+
+Imagine a sea of bits—an ocean of them. A typical modern computer has more than 30 billion bits in its volatile data storage (working memory). Nonvolatile storage (the hard disk or equivalent) tends to have yet a few orders of magnitude more.
+
+
+
+
+-- numbers 
+
+
+
+--- Automatic Type Conversion
+
+console.log(8 * null)
+// → 0
+console.log("5" - 1)
+// → 4
+console.log("5" + 1)
+// → 51
+console.log("five" * 2)
+// → NaN
+console.log(false == 0)
+// → true
+
+
+document.querySelector("#p1-name").textContent = "sahi" + "yadav" 
+
+// for accessing title in page
+
+document.title  
+
+
+// using index get the number of the word
+
+document.title.indexOf("T")
+
+document.title[9]
+
+document.querySelector("header h1").textContent.toLocaleUpperCase()
+
+
+
